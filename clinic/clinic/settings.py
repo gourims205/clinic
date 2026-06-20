72b6486587c12bca8f3fbd40d8984aa4a113f16a
Gouri M.S <gourims205@gmail.com>
2026-06-20 11:21:30 +0530

    Update settings.py

--- clinic/clinic/settings.py
+++ clinic/clinic/settings.py
@@ -118,7 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 STORAGES = {
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
