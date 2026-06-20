a204196474b28b42801e7852138069231d914035
Gouri M.S <gourims205@gmail.com>
2026-06-20 11:05:13 +0530

    Update settings.py

--- clinic/clinic/settings.py
+++ clinic/clinic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ev695pxhcrdg)9-p#(6$54(t4gto_#&r*6n59l4i=pclhj#qe&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://clinic-qzci.onrender.com',]
 
 
 # Application definition
